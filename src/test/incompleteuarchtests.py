b94dbf3586c712fab4fcf84762390c80cb7ccb45
Andrew Feldman <afeld2012@Andrews-MacBook-Air.local>
2022-11-05 05:05:13 -0400

    Unknown interface causes failure

--- src/test/incompleteuarchtests.py
+++ src/test/incompleteuarchtests.py
@@ -5,8 +5,33 @@ from util.taxonomy.expressions import *
 from util.taxonomy.designelement import *
 from util.taxonomy.rulesengine import *
 
-def genFormatUarch():
-    '''Helper function for format uarch testbench. Generate an example format uarch.'''
+def genPrimitiveMetadataParserWithUnknownAttributeAndInterfaceType(primitive_id):
+    '''Helper function for format uarch testbench. Generate a MetadataParser primitive.'''
+
+    # Category
+    primitive_category='MetadataParser'
+
+    # Attributes
+    primitive_attributes=[FormatType.fromIdValue('format','?')]
+
+    # Interface
+    # - md_in
+    net_type=NetType.fromIdValue('TestNetType','md')
+    format_type=FormatType.fromIdValue('TestFormatType','?')    
+    port_md_in=Port.fromIdDirectionNetTypeFormatType('md_in', 'in', net_type, format_type)    
+
+    # - at_bound_out
+    net_type=NetType.fromIdValue('TestNetType','pos')
+    format_type=FormatType.fromIdValue('TestFormatType','addr')
+    at_bound_out=Port.fromIdDirectionNetTypeFormatType('at_bound_out', 'out', net_type, format_type)
+
+    # - build interface
+    primitive_interface=[port_md_in,at_bound_out]
+
+    return Primitive.fromIdCategoryAttributesInterface(primitive_id, primitive_category, primitive_attributes, primitive_interface)
+
+def genFormatUarchWithHoleOrUnknownTypes(useTopologicalHole):
+    '''Helper function for incomplete format uarch testbench. Generate an example format uarch with either a topological hole, or a mix of unknown port and attribute types'''
 
     # Id
     component_id='TestFormatUarch'
@@ -36,12 +61,47 @@ def genFormatUarch():
     # - build interface
     component_interface=[port_md_in0,port_at_bound_out0,port_md_in1,port_at_bound_out1]
 
-   # Create topological hole
-    topology_id='TestTopologyHole'
-    component_topological_hole=Topology.holeFromId(id)
+    # Create topology
+    component_topology=[]
+
+    if useTopologicalHole:
+        # - Create topological hole
+        topology_id='TestTopologyHole'
+        component_topology=Topology.holeFromId(id)
+    else:
+    # Topology ID
+        topology_id='TestTopology'
+
+        # Component list setup
+        component_list=[genPrimitiveMetadataParserWithUnknownAttributeAndInterfaceType('TestMetadataParser0'),genPrimitiveMetadataParserWithUnknownAttributeAndInterfaceType('TestMetadataParser1')]
+
+        # Net list setup
+
+        # - Net connecting metadata (md) ports
+        net_type=NetType.fromIdValue('TestNetType','md')
+        format_type=FormatType.fromIdValue('TestFormatType','C')
+        port_id_list=['md_in0','TestMetadataParser0.md_in']
+        net_md0=Net.fromIdAttributes('TestMDNet0', net_type, format_type, port_id_list)
+        format_type=FormatType.fromIdValue('TestFormatType','B')
+        port_id_list=['md_in1','TestMetadataParser1.md_in']    
+        net_md1=Net.fromIdAttributes('TestMDNet0', net_type, format_type, port_id_list)
+
+        # - Net connecting position (pos) ports
+        net_type=NetType.fromIdValue('TestNetType','pos')
+        format_type=FormatType.fromIdValue('TestFormatType','addr')
+        port_id_list=['at_bound_out0','TestMetadataParser0.at_bound_out']
+        net_at_bound0=Net.fromIdAttributes('TestPosNet0', net_type, format_type, port_id_list)
+        port_id_list=['at_bound_out1','TestMetadataParser1.at_bound_out']    
+        net_at_bound1=Net.fromIdAttributes('TestPosNet1', net_type, format_type, port_id_list)
+
+        # - build Net list
+        net_list=[net_md0,net_md1,net_at_bound0,net_at_bound1]
+
+        # build topology
+        component_topology=Topology.fromIdNetlistComponentList(topology_id,net_list,component_list)
 
     # build component
-    component=Component.fromIdCategoryAttributesInterfaceTopology(component_id,component_category,component_attributes,component_interface,component_topological_hole)
+    component=Component.fromIdCategoryAttributesInterfaceTopology(component_id,component_category,component_attributes,component_interface,component_topology)
 
     return component
 
@@ -57,11 +117,14 @@ def do_tests():
     # Setup
 
     print("- Setup")
+
+    print("\n\n")
+
     print("-- Create format uarch component with a topological hole and [C,B] tensor ranks; print; dump")
-    format_uarch_component=genFormatUarch()
+    format_uarch_component=genFormatUarchWithHoleOrUnknownTypes(useTopologicalHole=True)
     import pprint
     pprint.pprint(str(format_uarch_component))
-    format_uarch_component.dump('incomplete_component_test.yaml')
+    format_uarch_component.dump('incomplete_component_hole_test.yaml')
 
     # Topology validation rules
 
@@ -81,3 +144,31 @@ def do_tests():
     print("\n\n")   
 
     rules_engine.run(format_uarch_component)
+
+    print("\n\n")
+
+    print("-- Create format uarch component with a primitives that have unknown interface and attribute types, and [C,B] tensor ranks; print; dump")
+    format_uarch_component=genFormatUarchWithHoleOrUnknownTypes(useTopologicalHole=False)
+    import pprint
+    pprint.pprint(str(format_uarch_component))
+    format_uarch_component.dump('incomplete_component_interface_attribute_type_test.yaml')
+
+    # Topology validation rules
+
+    print("\n\n")   
+
+    print("- Topology validation rules against incomplete format uarch")
+    base_rule_set_path='saftaxolib/base_ruleset'
+    primitive_md_parser_rule_set_path='saftaxolib/primitive_md_parser_ruleset'
+
+    print("\n\n")   
+
+    print("-- Load topology validation rules (",base_rule_set_path,",",primitive_md_parser_rule_set_path,") into RulesEngine & preload rules")
+    rules_engine = RulesEngine([base_rule_set_path,primitive_md_parser_rule_set_path])
+    rules_engine.preloadRules()
+    print("-- run()")
+
+    print("\n\n")   
+
+    rules_engine.run(format_uarch_component)    
+
