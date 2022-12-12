579c2182910b52ae922cb0eb8e3fbd4a7e143bb3
Andrew Feldman <afeld2012@Andrews-MacBook-Air.local>
2022-12-12 07:09:20 -0500

    Added delegation table.

--- src/util/sparseloop_config_processor.py
+++ src/util/sparseloop_config_processor.py
@@ -256,6 +256,8 @@ def bind_pgens(arch, mapping, prob):
     pgens={buffer:{dtype:[] for dtype in data_space_dict_list} for buffer in flat_arch}
     last_buffer={dtype:top_lvl_buffer for dtype in data_space_dict_list}
 
+    delegation_table={buffer:{dtype:buffer for dtype in data_space_dict_list} for buffer in flat_arch}
+
     for loop_buffer in flat_arch:
         loop_buffer_kept_data_spaces=buffer_loop_binding[loop_buffer]['data-spaces']
         loop_factors=buffer_loop_binding[loop_buffer]['loops']['factors']
@@ -265,13 +267,17 @@ def bind_pgens(arch, mapping, prob):
                 # (either this buffer or the lowest higher buffer which keeps dtype.)
                 last_buffer[dtype]=loop_buffer
             pgen_buffer=last_buffer[dtype]
+
+            delegation_table[loop_buffer][dtype]=pgen_buffer
             
             # Then, for each loop that is bound to loop_buffer AND projects onto dtype, create a pgen bound to that loop AND pgen_buffer.
             # Each loop is represented by a rank and the buffer it is bound to, rather than by the usual rank + tiling-level notation (i.e. M0, R2, etc.)
             dtype_projection_ranks=data_space_dict_list[dtype]['rank-list']
             pgens[pgen_buffer][dtype].extend([{'rank':rank,'loop_buffer':loop_buffer} for rank in loop_factors if rank in dtype_projection_ranks])
 
-    return pgens
+    print("Delegation table",delegation_table)
+
+    return pgens, delegation_table
 
 def first_unbound_nontrival_pgen_idx_by_rank(target_rank, buffer_dtype_pgens, nontrivial_pgen_ptrs, nontrivial_pgen_is_bound, update_is_bound=True):
     #print(target_rank)
@@ -305,7 +311,7 @@ def bind_format_iface(arch, mapping, prob, sparseopts):
     data_space_dict_list, prob_coeff_list, prob_instance_rank_sizes, prob_instance_densities=data_space_dict_list_from_sl_prob(prob)
     buffer_loop_binding=buffer_loop_binding_from_sl_arch_and_map(arch, mapping, prob_instance_rank_sizes, data_space_dict_list)
     flat_arch=flatten_arch_wrapper(arch)
-    pgens=bind_pgens(arch, mapping, prob)
+    pgens, delegation_table=bind_pgens(arch, mapping, prob)
     buffer_dataspace_to_fmt_layout_binding=get_buffer_dataspace_to_fmt_layout_bindings_from_sparseopts(sparseopts)
     buffer_dataspace_to_fmt_access_binding=get_buffer_dataspace_to_fmt_access_bindings_from_buffer_dataspace_to_fmt_layout_bindings(buffer_dataspace_to_fmt_layout_binding, data_space_dict_list, flat_arch, buffer_loop_binding)
 
@@ -450,6 +456,9 @@ def bind_format_iface(arch, mapping, prob, sparseopts):
 
     #print(fmt_ifaces)   
     return copy.deepcopy(fmt_ifaces)
+
+def bind_action_optimization(arch, mapping, prob, sparseopts):
+    pass
 
 """
 
