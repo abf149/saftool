b4b9fcf7d6ac9d83bc5ce5dfc88e7b37cdd7ac8e
Andrew Feldman <afeld2012@Andrews-MacBook-Air.local>
2023-01-04 23:43:08 -0500

    WIP on skipping uarch topology rewrite rules

--- src/saftaxolib/skipping_uarch_ruleset/RuleSetContextModule.py
+++ src/saftaxolib/skipping_uarch_ruleset/RuleSetContextModule.py
@@ -53,18 +53,6 @@ def genSkippingUarchWithHole(skipping_uarch_id):
 
     return component
 
-'''
-def formatUarchPortIdToBufferPortIdMapping(format_uarch_port_id):
-    format_uarch_port_prefix_list=['md_in','at_bound_out']
-    buffer_port_prefix_list=['md_out','at_bound_in']
-
-    for format_uarch_port_prefix,buffer_port_prefix in zip(format_uarch_port_prefix_list,buffer_port_prefix_list):
-        if format_uarch_port_id.startswith(format_uarch_port_prefix):
-            port_idx=format_uarch_port_id.split(format_uarch_port_prefix)[1]
-            buffer_port_id=buffer_port_prefix+port_idx
-            return buffer_port_id
-'''
-
 def concretizeArchitectureSkippingSAFsToSkippingUarches(obj):
     '''Object is an architecture with at least one skipping SAF; concretize skipping SAF(s) to skipping uarch(es) and critically, DELETE THE SKIPPING SAFS'''
     
@@ -145,6 +133,111 @@ def predicateIsArchitectureHasSkippingSAF(obj):
     '''Object is an architecture with at least one skipping SAF'''
     print(obj.getId())
     return type(obj).__name__ == 'Architecture' and ('skipping' in [saf.getCategory() for saf in obj.getSAFList()])
+
+# -- TransformTopologicalHoleToSkippingUarchTopology
+# --- rewrite rule
+
+def genPrimitivePgen():
+    '''Helper function for format uarch testbench. Generate a MetadataParser primitive.'''
+
+    # Category
+    primitive_category='MetadataParser'
+
+    # Attributes
+    primitive_attributes=[FormatType.fromIdValue('format',fmt)]
+
+    # Interface
+    # - md_in
+    net_type=NetType.fromIdValue('TestNetType','md')
+    format_type=FormatType.fromIdValue('TestFormatType',fmt)    
+    port_md_in=Port.fromIdDirectionNetTypeFormatType('md_in', 'in', net_type, format_type)    
+
+    # - at_bound_out
+    net_type=NetType.fromIdValue('TestNetType','pos')
+    format_type=FormatType.fromIdValue('TestFormatType','addr')
+    at_bound_out=Port.fromIdDirectionNetTypeFormatType('at_bound_out', 'out', net_type, format_type)
+
+    # - build interface
+    primitive_interface=[port_md_in,at_bound_out]
+
+    return Primitive.fromIdCategoryAttributesInterface(primitive_id, primitive_category, primitive_attributes, primitive_interface)
+
+def genPrimitiveIntersect():
+    '''Helper function for format uarch testbench. Generate a MetadataParser primitive.'''
+
+    # Category
+    primitive_category='MetadataParser'
+
+    # Attributes
+    primitive_attributes=[FormatType.fromIdValue('format',fmt)]
+
+    # Interface
+    # - md_in
+    net_type=NetType.fromIdValue('TestNetType','md')
+    format_type=FormatType.fromIdValue('TestFormatType',fmt)    
+    port_md_in=Port.fromIdDirectionNetTypeFormatType('md_in', 'in', net_type, format_type)    
+
+    # - at_bound_out
+    net_type=NetType.fromIdValue('TestNetType','pos')
+    format_type=FormatType.fromIdValue('TestFormatType','addr')
+    at_bound_out=Port.fromIdDirectionNetTypeFormatType('at_bound_out', 'out', net_type, format_type)
+
+    # - build interface
+    primitive_interface=[port_md_in,at_bound_out]
+
+    return Primitive.fromIdCategoryAttributesInterface(primitive_id, primitive_category, primitive_attributes, primitive_interface)
+
+def generateSkippingUarchTopology():
+   # Topology ID
+    topology_id='TestTopology'
+
+    # Component list setup
+    component_list=[]
+    for idx in range(len(rank_format_strs)):
+        fmt_str=rank_format_strs[idx]
+        component_list.append(genPrimitiveMetadataParser('TestMetadataParser'+str(idx), fmt_str))
+        
+
+    # Net list setup
+
+    # - Nets connecting metadata (md) ports to primitives, position (pos) ports to primitives
+    net_list=[]
+    for idx in range(len(rank_format_strs)): 
+        fmt_str=rank_format_strs[idx]        
+        # Metadata net
+        net_type=NetType.fromIdValue('TestNetType','md')
+        format_type=FormatType.fromIdValue('TestFormatType',fmt_str)
+        port_id_list=['md_in'+str(idx),'TestMetadataParser'+str(idx)+'.md_in']
+        net_md=Net.fromIdAttributes('TestMDNet'+str(idx), net_type, format_type, port_id_list)
+        # Position net
+        net_type=NetType.fromIdValue('TestNetType','pos')
+        format_type=FormatType.fromIdValue('TestFormatType','addr')
+        port_id_list=['at_bound_out'+str(idx),'TestMetadataParser'+str(idx)+'.at_bound_out']
+        net_at_bound=Net.fromIdAttributes('TestPosNet'+str(idx), net_type, format_type, port_id_list)
+        net_list.append(net_md)
+        net_list.append(net_at_bound)
+
+    # build topology
+    return Topology.fromIdNetlistComponentList(topology_id,net_list,component_list)
+
+def transformTopologicalHoleToSkippingUarchTopology(obj):
+    '''Fill the topological hole with a leader-follower skipping uarch topology. TODO: support bidirectional'''
+
+    obj.setTopology(generateSkippingUarchTopology())
+
+    return obj
+
+def predicateIsComponent(obj):
+    return type(obj).__name__ == 'Component'
+
+def predicateIsSkippingUarch(obj):
+    return obj.getCategory() == 'SkippingUarch'
+
+def predicateHasTopologicalHole(obj):
+    return obj.getTopology().isHole()
+
+def predicateIsComponentIsSkippingUarchHasTopologicalHole(obj):
+    return predicateIsComponent(obj) and predicateIsSkippingUarch(obj) and predicateHasTopologicalHole(obj)
 
 """ # - Format uarch rewrite rules
 
