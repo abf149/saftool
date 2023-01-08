13dd7620ccc02292f90c1a2d8dd546a58801bb2e
Andrew Feldman <afeld2012@Andrews-MacBook-Air.local>
2023-01-08 13:05:02 -0500

    test script for accelergy primitives

--- src/accelergy/test/test_accelergy.sh
+++ src/accelergy/test/test_accelergy.sh
@@ -1,4 +1,7 @@
 # accelergyTables may need to be run once
 accelergyTables
 
+# Clear prior outputs
+rm ref_outputs/*
+
 accelergy -o ref_outputs/ ref_inputs/arch/*.yaml ref_inputs/components/*.yaml ./ref_inputs/*.yaml -v 1
