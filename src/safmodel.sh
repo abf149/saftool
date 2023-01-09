5d9a495cef81af3e821793602b9fcbb6bb0a041d
Andrew Feldman <afeld2012@Andrews-MacBook-Air.local>
2023-01-09 05:36:26 -0500

    wip on the more involved approach to safmodel

---
+++ src/safmodel.sh
@@ -0,0 +1,15 @@
+PYTHON=python3
+EXEC_DIR=$(pwd)
+SAFTOOLS_DIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+
+
+echo "cd ${SAFTOOLS_DIR}"
+cd ${SAFTOOLS_DIR}
+
+pwd
+
+${PYTHON} -m safmodel "$@"
+
+echo "cd ${EXEC_DIR}"
+cd ${EXEC_DIR}
