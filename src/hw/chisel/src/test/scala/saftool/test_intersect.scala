d5000ed083cab18d4adf05aa3dc2a54ef7f45fee
Andrew Feldman <afeld2012@Andrews-MacBook-Air.local>
2023-01-18 21:54:23 -0500

    refactored hw scala code

--- src/hw/chisel/src/test/scala/saftool/test_intersect.scala
+++ src/hw/chisel/src/test/scala/saftool/test_intersect.scala
@@ -2,37 +2,95 @@
 
 package saftool
 
+import saftool._
+import chisel3._
+import chisel3.util._
+import chiseltest._
+import chiseltest.iotesters._
+import treadle.WriteVcdAnnotation
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.flatspec.AnyFlatSpec
 import java.nio.file.{Paths, Files}
 import java.nio.charset.StandardCharsets
 
-import chisel3.stage.ChiselStage
-import chisel3.Input
-import chisel3.util._
-//import chisel3.iotesters.{ChiselFlatSpec, Driver, PeekPokeTester}
+class Workload_BidirectionalCoordinatePayloadIntersectDecoupled_Default(intersect: BidirectionalCoordinatePayloadIntersectDecoupled) extends PeekPokeTester(intersect) {
+  poke(intersect.input0.valid,0)  
+  poke(intersect.input1.valid,0) 
+  reset(2)
+  step(3)
+  expect(intersect.input0.ready, 1, s"intersect.input0.ready expected: 1, got: ${peek(intersect.input0.ready)}")
+  expect(intersect.input1.ready, 1, s"intersect.input0.ready expected: 1, got: ${peek(intersect.input0.ready)}")  
+  poke(intersect.input0.bits.in,1)
+  poke(intersect.input0.valid,1)  
+  poke(intersect.input1.bits.in,1)  
+  poke(intersect.input1.valid,1)     
+  step(1)
+  poke(intersect.input0.valid,0)  
+  poke(intersect.input1.valid,0)     
+  poke(intersect.output.ready,1)
 
-import chisel3._
-import chiseltest._
-import org.scalatest.freespec.AnyFreeSpec
-import chisel3.experimental.BundleLiterals._
+  while (peek(intersect.output.valid) == 0) {
+    step(1)
+  }
 
-import saftool._
+  poke(intersect.output.ready,0)
+}
+
+class Workload_BidirectionalBitmaskIntersectDecoupled_Default(intersect: BidirectionalBitmaskIntersectDecoupled) extends PeekPokeTester(intersect) {
+  poke(intersect.input0.valid,0)  
+  poke(intersect.input1.valid,0) 
+  reset(2)
+  step(3)
+  expect(intersect.input0.ready, 1, s"intersect.input0.ready expected: 1, got: ${peek(intersect.input0.ready)}")
+  expect(intersect.input1.ready, 1, s"intersect.input0.ready expected: 1, got: ${peek(intersect.input0.ready)}")  
+  poke(intersect.input0.bits.in,Integer.parseInt("01010101", 2)) //85
+  poke(intersect.input0.valid,1)  
+  poke(intersect.input1.bits.in,Integer.parseInt("11001101", 2)) //205
+  poke(intersect.input1.valid,1)     
+  step(1)
+  poke(intersect.input0.valid,0)  
+  poke(intersect.input1.valid,0)     
+  poke(intersect.output.ready,1)
+
+  while (peek(intersect.output.valid) == 0) {
+    step(1)
+  }
+
+  poke(intersect.output.ready,0) // Expect Integer.parseInt("01000101", 2)) // 69
+}
+
+class Test_Sim_BidirectionalCoordinatePayloadIntersectDecoupled extends AnyFlatSpec with ChiselScalatestTester {
+  behavior of "BidirectionalCoordinatePayloadIntersectDecoupled"
+  it should "default_workload" in {
+    test(new BidirectionalCoordinatePayloadIntersectDecoupled(metaDataWidth = 8)).withAnnotations(Seq(WriteVcdAnnotation)).runPeekPoke(new Workload_BidirectionalCoordinatePayloadIntersectDecoupled_Default(_))
+  }
+
+  behavior of "BidirectionalBitmaskIntersectDecoupled"
+  it should "default_workload" in {
+    test(new BidirectionalBitmaskIntersectDecoupled(metaDataWidth = 8)).withAnnotations(Seq(WriteVcdAnnotation)).runPeekPoke(new Workload_BidirectionalBitmaskIntersectDecoupled_Default(_))
+  }  
+}
 
 class Test_Emit_Intersection extends AnyFreeSpec with ChiselScalatestTester {
 
-  "Test_Emit_Intersection" in {
-    test(new BidirectionalCoordinatePayloadIntersectDecoupled(8)) { dut =>
+  var verilog_dir="src/verilog/"
 
-      var x=chisel3.getVerilogString(new BidirectionalCoordinatePayloadIntersectDecoupled(metaDataWidth = 8))
+  "Emit_BidirectionalCoordinatePayloadIntersectDecoupled" in {
+      var filename="BidirectionalCoordinatePayloadIntersectDecoupled.v"
+      var intersect=chisel3.getVerilogString(new BidirectionalCoordinatePayloadIntersectDecoupled(metaDataWidth = 8))
+      Files.write(Paths.get(verilog_dir+filename), intersect.getBytes(StandardCharsets.UTF_8))
+  }
 
-      //new Help_Test_Do_Intersection(dut)
+  "Emit_BidirectionalBitmaskIntersectDecoupled" in {
+      var filename="BidirectionalBitmaskIntersectDecoupled.v"
+      var intersect=chisel3.getVerilogString(new BidirectionalBitmaskIntersectDecoupled(metaDataWidth = 8))
+      Files.write(Paths.get(verilog_dir+filename), intersect.getBytes(StandardCharsets.UTF_8))      
+  }
 
-      //print(x)
-      Files.write(Paths.get("src/verilog/IntersectFmtCDirBidirDecoupled.v"), x.getBytes(StandardCharsets.UTF_8))
 
-      x=chisel3.getVerilogString(new BidirectionalBitmaskIntersectDecoupled(metaDataWidth = 8))
-      //print(x)
-      Files.write(Paths.get("src/verilog/IntersectFmtBDirBidirDecoupled.v"), x.getBytes(StandardCharsets.UTF_8))      
-
+  "Emit_LeaderFollowerCoordinatePayloadIntersectDecoupled" in {  
+      var filename="LeaderFollowerCoordinatePayloadIntersectDecoupled.v"
       /*
       x=chisel3.getVerilogString(new LeaderFollowerCoordinatePayloadFrontendDecoupled(metaDataWidth = 8))
       print(x)
@@ -44,17 +102,5 @@ class Test_Emit_Intersection extends AnyFreeSpec with ChiselScalatestTester {
       print(x)
       Files.write(Paths.get("src/verilog/IntersectFmtCDirLFDecoupled.v"), x.getBytes(StandardCharsets.UTF_8))
       */
-
-      /*
-      x=chisel3.getVerilogString(new AUX_Intersect_Format_B_MDOrch_Uncoupled(mdTileSize = 8))
-      print(x)
-      Files.write(Paths.get("src/verilog/AUX_Intersect_Format_B_MDOrch_Uncoupled.v"), x.getBytes(StandardCharsets.UTF_8))
-
-      x=chisel3.getVerilogString(new AUX_Intersect_Format_C_MDOrch_Coupled(metaDataWidth = 8))
-      print(x)
-      Files.write(Paths.get("src/verilog/AUX_Intersect_Format_C_MDOrch_Coupled.v"), x.getBytes(StandardCharsets.UTF_8))      
-      */
-
-    }
   }
 }
