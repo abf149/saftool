e1b429ac28ea8cde31c9ed59be0d7746c2311030
Andrew Feldman <afeld2012@Andrews-MacBook-Air.local>
2023-02-19 18:44:45 -0500

    added multiplier to basic tests

--- src/hw/chisel/src/main/scala/saftool/BasicTest.scala
+++ src/hw/chisel/src/main/scala/saftool/BasicTest.scala
@@ -27,3 +27,23 @@ class RegisteredAdder(bitwidth: Int) extends Module  with RequireSyncReset {
   out_reg := inL_reg + inR_reg
   io.out := out_reg
 }
+
+class RegisteredMultiplier(bitwidth: Int) extends Module  with RequireSyncReset {
+  val io = IO(new Bundle {
+    val inL = Input(UInt(bitwidth.W))
+    val inR = Input(UInt(bitwidth.W))
+    val out = Output(UInt(bitwidth.W))
+  })
+
+  // Registered inputs
+  val inL_reg = RegInit(0.U) // TODO: DontCare
+  val inR_reg = RegInit(0.U)    
+
+  // Registered outputs
+  val out_reg = RegInit(0.U)
+
+  inL_reg := io.inL
+  inR_reg := io.inR
+  out_reg := inL_reg * inR_reg
+  io.out := out_reg
+}
