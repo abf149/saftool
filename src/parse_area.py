6dc7633c3555bd4c6149ae91073d6e92116e062b
Andrew Feldman <afeld2012@Andrews-MacBook-Air.local>
2023-05-27 01:07:38 -0400

    parse_area.py update

--- src/parse_area.py
+++ src/parse_area.py
@@ -4,20 +4,32 @@
 def parse_area(fn):
     area_unit='um2'
 
+    res={}
+
     with open(fn) as f:
         lns=f.readlines()
 
         found_area=False
+        found_divider=False
 
         for ln in lns:
-            print(ln)
-
+            
             if found_area:
-                pass
+                if found_divider:
+                    if "----" in ln:
+                        return res
+                    else:
+                        ln_strip=ln.strip()
+                        field_value=ln_strip.split(":") #(field,value)
+                        fld=field_value[0].strip().replace(" ","_")
+                        vl=float(field_value[1].strip())
+                        res[fld]=vl
+                else:
+                    if "----" in ln:
+                        found_divider=True
             else:
-                print("strip:",ln.strip())
                 if ln.strip()=="Area":
-                    print("Detected.")
+                    found_area=True
 
 
 
