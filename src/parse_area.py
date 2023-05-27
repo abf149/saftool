91690c7f12d10f41167a7cccc84012c37586bed9
Andrew Feldman <afeld2012@Andrews-MacBook-Air.local>
2023-05-27 01:07:55 -0400

    refactoring

--- src/parse_area.py
+++ src/parse_area.py
@@ -30,26 +30,3 @@ def parse_area(fn):
             else:
                 if ln.strip()=="Area":
                     found_area=True
-
-
-
-        # found_hierarchy=False
-        # found_divider=False
-        # for ln in lns:
-
-        #     if found_divider:
-        #         flds_str=ln.strip().split()
-        #         flds=[]
-        #         for fld_str in flds_str:
-        #             try:
-        #                 flds.append(float(fld_str))
-        #             except:
-        #                 pass
-        #         res = [flds[0]]
-        #         res.extend(flds[2:])
-        #         return res, area_unit
-
-        #     if found_hierarchy and '-' in ln:
-        #         found_divider=True
-        #     if 'Hierarchical' in ln:
-        #         found_hierarchy=True
