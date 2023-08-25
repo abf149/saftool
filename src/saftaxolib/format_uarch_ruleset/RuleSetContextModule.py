556cdb0d1b253e5b8b45564470010b8fb95e2fd5
Andrew Feldman <afeld2012@Andrews-MacBook-Air.local>
2023-08-24 21:08:37 -0400

    refactoring

--- src/saftaxolib/format_uarch_ruleset/RuleSetContextModule.py
+++ src/saftaxolib/format_uarch_ruleset/RuleSetContextModule.py
@@ -50,62 +50,6 @@ transformTopologicalHoleToPerRankMdParserTopology = \
                                                                             fmt_uarch_topologies, \
                                                                             FormatUarch)
 
-def generateFormatUarchTopology(rank_format_strs):
-   # Topology ID
-    topology_id='TestTopology'
-
-    # Component list setup
-    component_list=[]
-    for idx in range(len(rank_format_strs)):
-        fmt_str=rank_format_strs[idx]
-        component_list.append(MetadataParser.copy().set_attribute('format', \
-                                               FormatType.fromIdValue('format',fmt_str)
-                                              ).build('TestMetadataParser'+str(idx)))
-
-    # Net list setup
-
-    # - Nets connecting metadata (md) ports to primitives, position (pos) ports to primitives
-    net_list=[]
-    for idx in range(len(rank_format_strs)): 
-        fmt_str=rank_format_strs[idx]        
-        # Metadata net
-        net_type=NetType.fromIdValue('TestNetType','md')
-        format_type=FormatType.fromIdValue('TestFormatType',fmt_str)
-        port_id_list=['md_in'+str(idx),'TestMetadataParser'+str(idx)+'.md_in']
-        net_md=Net.fromIdAttributes('TestMDNet'+str(idx), net_type, format_type, port_id_list)
-        # Position net
-        net_type=NetType.fromIdValue('TestNetType','pos')
-        format_type=FormatType.fromIdValue('TestFormatType','addr')
-        port_id_list=['at_bound_out'+str(idx),'TestMetadataParser'+str(idx)+'.at_bound_out']
-        net_at_bound=Net.fromIdAttributes('TestPosNet'+str(idx), net_type, format_type, port_id_list)
-        net_list.append(net_md)
-        net_list.append(net_at_bound)
-
-    # build topology
-    return Topology.fromIdNetlistComponentList(topology_id,net_list,component_list)
-
-'''
-def transformTopologicalHoleToPerRankMdParserTopology(obj):
-    #Fill the topological hole with a topology comprising one MetadataParser primitive per traversed tensor rank at this buffer level
-    rank_format_strs = [fmt_type['value'] for fmt_type in obj.attributes[0]]
-    obj.setTopology(generateFormatUarchTopology(rank_format_strs))
-    return obj
-'''
-
-def predicateIsComponent(obj):
-    return type(obj).__name__ == 'Component'
-
-def predicateIsFormatUarch(obj):
-    return obj.getCategory() == 'FormatUarch'
-
-def predicateHasTopologicalHole(obj):
-    return obj.getTopology().isHole()
-
-'''
-def predicateIsComponentIsFormatUarchHasTopologicalHole(obj):
-    return predicateIsComponent(obj) and predicateIsFormatUarch(obj) and predicateHasTopologicalHole(obj)
-'''
-
 # - MetadataParser validation rules
 # -- AssertPrimitiveMetadataParserSupportedInstantiation: MetadataParser instance must be supported
 predicateIsPrimitiveMetadataParser, \
