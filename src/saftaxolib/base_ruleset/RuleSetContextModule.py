34be24c36871a10c6002dc36c87dc010cd8cc7bb
Andrew Feldman <afeld2012@Andrews-MacBook-Air.local>
2023-08-25 15:24:14 -0400

    refactoring

--- src/saftaxolib/base_ruleset/RuleSetContextModule.py
+++ src/saftaxolib/base_ruleset/RuleSetContextModule.py
@@ -58,34 +58,3 @@ predicateIsPrimitiveOrComponentHasUnknownAttributeTypeAndKnownInterfaceTypeRefer
     b_.AND(b_.OR(p_.isPrimitive, \
                  p_.isComponent), \
            p_.hasKnownInterfaceTypeReferencingUnknownAttribute)
-
-# - MetadataParser rewrite rules
-'''
-def transformUnknownAttributeTypeFromInterfaceType(obj):
-    attribute_unknown=obj.getAttributeById('format').isUnknown()
-
-    interface_type=obj.getPortById('md_in').getFormatType().getValue()
-
-    # TODO: make a real read/modify/write for attributes
-    atts=obj.getAttributes()
-    for idx in range(len(atts)):
-        if type(atts[idx]).__name__=='FormatType' and atts[idx].getId()=='format':
-            atts[idx].setValue(interface_type)
-    obj.setAttributes(atts)
-    return obj
-'''
-
-#predicateIsPrimitiveOrComponent
-
-'''
-predicateIsPrimitiveMetadataParserHasUnknownAttributeTypeAndKnownInterfaceType = \
-    b_.AND(p_.isPrimitive, \
-           lambda obj: p_.isCategory(obj,"MetadataParser"), \
-           b_.NOT(lambda obj: obj.getPortById('md_in').getFormatType().isUnknown()), \
-           lambda obj: obj.getAttributeById('format').isUnknown())
-'''
-
-'''
-def predicateIsPrimitiveMetadataParserHasUnknownAttributeTypeAndKnownInterfaceType(obj):
-    return predicateIsPrimitiveMetadataParser(obj) and (not obj.getPortById('md_in').getFormatType().isUnknown()) and obj.getAttributeById('format').isUnknown()
-'''
