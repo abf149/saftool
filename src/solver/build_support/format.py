08bda93d162235b86fa0be2717f4e31677365e39
Andrew Feldman <afeld2012@Andrews-MacBook-Air.local>
2023-11-29 23:54:04 -0500

    SAFmodel skipping works for output! And no unnecessary format uarch for dense ranks

--- src/solver/build_support/format.py
+++ src/solver/build_support/format.py
@@ -4,20 +4,45 @@ import solver.build_support.arch as ar_
 from util.helper import info,warn,error
 
 def get_buffer_stubs_and_format_safs(arch, fmt_iface_bindings, buffer_stub_list=[], saf_list=[]):
+    info("-- Generating buffer stubs and Format SAFs")
     buffer_stub_list=[]
     saf_list=[]
     buffer_hierarchy=ar_.get_buffer_hierarchy(arch)    
     for buffer in buffer_hierarchy:
+        info("--- Buffer:",buffer)
         datatype_fmt_ifaces=fmt_iface_bindings[buffer]
 
         if sum([len(datatype_fmt_ifaces[dtype]) for dtype in datatype_fmt_ifaces])>0:
-            fmt_saf=FormatSAF.copy() \
-                             .target(buffer) \
-                             .set_attribute("fibertree",datatype_fmt_ifaces,"fibertree")
+            info("---- Format interface bindings:",datatype_fmt_ifaces)
+            if any([(len(datatype_fmt_ifaces[dtype])>0 and (not datatype_fmt_ifaces[dtype][0]['dense'])) \
+                    for dtype in datatype_fmt_ifaces]):
+                info('---- Generating FormatSAF')
+                info("")
+                fmt_saf=FormatSAF.copy() \
+                                .target(buffer) \
+                                .set_attribute("fibertree",datatype_fmt_ifaces,"fibertree")
+                info("fmt_saf=FormatSAF.copy()")
+                info(".target(",buffer,")")
+                info(".set_attribute(\'fibertree\',",datatype_fmt_ifaces,",\'fibertree\')")
+                info("")
+                warn("---- => Done, generating FormatSAF")
+                saf_list.append(("format_saf",fmt_saf))  
+            else:
+                warn("---- Buffer",buffer,"has only dense format interfaces; not generating FormatSAF")
 
+            info("---- Generating BufferStub")
+            info("")
             buffer_stub=BufferStub.copy() \
                                   .set_attribute("fibertree",datatype_fmt_ifaces,"fibertree") \
                                   .generate_ports("fibertree","fibertree")
+            info("buffer_stub=BufferStub.copy()")
+            info(".set_attribute(\'fibertree\',",datatype_fmt_ifaces,",\'fibertree\')")
+            info(".generate_ports(\'fibertree\',\'fibertree\')")
+            info("")
+            warn("---- => Done, generating BufferStub")
             buffer_stub_list.append((buffer,buffer_stub))
-            saf_list.append(("format_saf",fmt_saf))    
+        else:
+            warn("---- Buffer",buffer,"has no format interfaces; skipping")
+  
+    warn("-- => Done, generating buffer stubs and Format SAFs")
     return buffer_stub_list, saf_list
